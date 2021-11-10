dc3ebba562fa591084f4ecae4ccddadbe6744dfe
BlackIcePenguin <bonneyjack10@gmail.com>
2021-11-10 09:22:06 -0500

    Added yellow fish that take away points, start screen and end screen, though the end screen is bugging, and I am not sure why.

--- Collision_Project_Audio_And_Picture.py
+++ Collision_Project_Audio_And_Picture.py
@@ -35,13 +35,17 @@ background_img = pygame.image.load("Clouds.png")
 screen = pygame.display.set_mode(SIZE)
 pygame.display.set_caption('Pygame Lab')
 FONT = pygame.font.SysFont('Calibri', 25, True, False)
-fish_sound = pygame.mixer.Sound("completetask_0.mp3")
-fish2 = pygame.image.load("Fish2.png")
+gain_point = pygame.mixer.Sound("completetask_0.mp3")
+loose_point = pygame.mixer.Sound('damage_sound.wav')
+good_fish = pygame.image.load("Fish2.png")
+bad_fish = pygame.image.load("YellowFish.png")
 bgm = pygame.mixer.Sound("river_mastered.mp3")
 OCEAN_BLUE_CLEAR = pygame.Color(26, 117, 255, a=0)
 clock = pygame.time.Clock()
 
+beginning = True
 running = True
+final = True
 
 
 # Setting up the classes
@@ -174,7 +178,7 @@ class Box:
         if self.x < 0:
             self.y = random.randrange(250, SCREEN_HEIGHT, 25)
             self.x = random.randrange(700, 800, 15)
-            self.x_speed = random.randint(1, 3)
+            self.x_speed = random.randint(1, 4)
         self.x -= self.x_speed
 
     def reset(self):
@@ -188,7 +192,6 @@ class Box:
                 for y_val in range(-self.height, 20):
                     if self.y - y_val == other.y:
                         other.reset()
-                        fish_sound.play()
                         return True
                     else:
                         pass
@@ -197,16 +200,18 @@ class Box:
 
 
 class Score:
-    def __init__(self, display):
+    def __init__(self, display, x, y):
         self.display = display
+        self.x = x
+        self.y = y
 
     def draw_score(self, value):
         score_value = FONT.render(f'Fish Caught : {value}', True, BLACK)
-        self.display.blit(score_value, [540, 10])
+        self.display.blit(score_value, [self.x, self.y])
 
     def draw_timer(self):
         time_text = FONT.render(f'Time taken : {int(time_count)}', True, BLACK)
-        self.display.blit(time_text, [540, 60])
+        self.display.blit(time_text, [self.x, self.y + 50])
 
 
 class EndScreen:
@@ -218,37 +223,65 @@ class EndScreen:
 
     def game_end(self):
         end_text = FONT.render(f'You caught 100 fish in {int(time_count)} seconds, good job!', True, WHITE)
+        kill_text = FONT.render('Press any key to quit', True, WHITE)
         self.display.fill(self.color)
         self.display.blit(end_text, [self.x, self.y])
+        self.display.blit(kill_text, [self.x + 20, self.y + 100])
 
 
 person = Person(100, 300, PEACH)
 raft = Raft(100, 300)
 ocean = Ocean(500, 250)
 sky = Sky(300, 100)
-fish_caught = Score(screen)
-player_width = 100
+fish_caught = Score(screen, 530, 10)
+player_width = 70
 player_height = 72
-player = Box(screen, raft.x, raft.y, player_width, player_height, BLACK, "Null")
+player = Box(screen, raft.x + 30, raft.y, player_width, player_height, BLACK, "Null")
 enemy_width = 20
-enemy_list = []
+point_enemy_list = []
+bad_enemy_list = []
 counter = 0
 time_count = 0
-end = EndScreen(screen, 130, 240, OCEAN_BLUE)
-at_end = False
+# end = EndScreen(screen, 130, 240, OCEAN_BLUE)
 bgm.play(True)
 for i in range(15):
     y_coord = random.randrange(250, SCREEN_HEIGHT, 15)
     random_x = random.randrange(700, 900, 5)
-    enemy_list.append(Box(screen, random_x, y_coord, enemy_width, enemy_width, OCEAN_BLUE_CLEAR, fish2))
+    point_enemy_list.append(Box(screen, random_x, y_coord, enemy_width, enemy_width, OCEAN_BLUE_CLEAR, good_fish))
+for i in range(5):
+    y_coord = random.randrange(250, SCREEN_HEIGHT, 15)
+    random_x = random.randrange(700, 900, 5)
+    bad_enemy_list.append(Box(screen, random_x, y_coord, enemy_width, enemy_width, OCEAN_BLUE_CLEAR, bad_fish))
+while beginning:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            beginning = False
+            running = False
+            final = False
+        if event.type == pygame.KEYDOWN:
+            beginning = False
+
+    screen.fill(SKY_BLUE)
+    goal_text = FONT.render(f'The game ends when you get 100 points', True, BLACK)
+    goal2_text = FONT.render(F'Red fish are worth 1 point, while yellow take away 2!', True, BLACK)
+    goal3_text = FONT.render('Try to be as fast as possible', True, BLACK)
+    control_text = FONT.render('Move by using the up and down arrow keys', True, BLACK)
+    control2_text = FONT.render('Press any key to start the game', True, BLACK)
+    start_text = [goal_text, goal2_text, goal3_text, control_text, control2_text]
+    for item in start_text:
+        screen.blit(item, [80, 100 + (75 * start_text.index(item))])
+    pygame.display.flip()
+
+    clock.tick(FPS)
 while running:
-    # Get all input events (Keyboard, Mouse, Joystick, etc
     time_count += (1/60)
+    # Get all input events (Keyboard, Mouse, Joystick, etc
     for event in pygame.event.get():
 
         # Look for specific event
         if event.type == pygame.QUIT:
             running = False
+            final = False
         if event.type == pygame.KEYDOWN:
             if event.key == pygame.K_UP:
                 raft.y_speed = -2
@@ -279,37 +312,53 @@ while running:
     ocean.main_ocean()
     ocean.flow()
     sky.sun()
-    # sky.cloud()
     player.update()
     raft.raft_move()
-    player.draw_box()
+
     raft.raft_draw()
+    player.draw_box()
     person.person_move()
     person.draw_person_on_raft()
-    for enemy in enemy_list:
+    for enemy in point_enemy_list:
         enemy.draw_box()
         enemy.draw_triangle()
         enemy.move_box()
         if player.is_collided(enemy):
+            gain_point.play()
             counter += 1
+    for enemy in bad_enemy_list:
+        enemy.draw_box()
+        enemy.draw_triangle()
+        enemy.move_box()
+        if player.is_collided(enemy):
+            loose_point.play()
+            counter -= 2
     fish_caught.draw_score(counter)
     fish_caught.draw_timer()
     if counter >= 100:
-        end.game_end()
-        pygame.display.flip()
-        at_end = True
         running = False
 
     pygame.display.flip()
 
     clock.tick(FPS)
 
-if at_end:
-    end_clock = 0
-    while end_clock < 10000:
-        end_clock += 0.0005
 
 # Runs when main game loop ends
+while final:
+    for event in pygame.event.get():
+        if event == pygame.quit():
+            final = False
+        if event == pygame.KEYDOWN:
+            final = False
+
+    end_text = FONT.render(f'You caught 100 fish in {int(time_count)} seconds, good job!', True, WHITE)
+    kill_text = FONT.render('Press any key to quit', True, WHITE)
+    screen.fill(OCEAN_BLUE)
+    screen.blit(end_text, [130, 240])
+    screen.blit(kill_text, [150 + 20, 290])
+    pygame.display.flip()
+    clock.tick(FPS)
+
 pygame.quit()
 
 # Credits
